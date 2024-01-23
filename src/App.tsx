7b61ce2b8304824cb14d91bb812431603f5865d4
Breno Menezes <breno.luizmenezes0123@gmail.com>
2024-01-22 22:58:15 -0300

    Cards de temas adicionados e completados

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import {AuthProvider} from "./contexts/AuthContext";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
-import {AuthProvider} from "./contexts/AuthContext";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
 
 
 function App() {
@@ -16,10 +19,14 @@ function App() {
             <Navbar />
             <div className='min-h-[80vh]'>
               <Routes>
-                <Route path="/" element={<Cadastro />} />
+                <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/temas" element={<ListaTemas />} />
+                <Route path="/cadastroTema" element={<FormularioTema />} />
+                <Route path="/editarTema/:id" element={<FormularioTema />} />
+                <Route path="/deletarTema/:id" element={<DeletarTema />} />
               </Routes>
             </div>
             <Footer />
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,15 +1,20 @@
+import {useContext} from 'react';
+import {UserContext} from "../../contexts/UserContext";
 import homeLogo from '../../assets/home.svg'
 import './Home.css';
 
 function Home() {
+  const {nome} = useContext(UserContext);
+
+  //console.log("Valor de nome: ", nome);
   return (
     <>
       <div className="bg-indigo-900 flex justify-center">
         <div className='container grid grid-cols-2 text-white'>
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-            <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-
+            <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
+            <h2 className="text-3x1 font-bold">{nome}</h2>
+            <p>Expresse aqui seus pensamentos e opiniões</p>
             <div className="flex justify-around gap-4">
 
               <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
